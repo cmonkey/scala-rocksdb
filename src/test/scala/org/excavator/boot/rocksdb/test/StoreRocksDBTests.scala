e08eead74b37ae16021293894491acce231e2b55
CMonkey <42.codemonkey@gmail.com>
2019-12-11 15:27:14 +0800

    add StoreRocksDBTests.testRocksDBStatus

--- src/test/scala/org/excavator/boot/rocksdb/test/StoreRocksDBTests.scala
+++ src/test/scala/org/excavator/boot/rocksdb/test/StoreRocksDBTests.scala
@@ -1,5 +1,21 @@
 package org.excavator.boot.rocksdb.test
 
+import org.excavator.boot.rocksdb.StoreRocksDB
+import org.junit.jupiter.api.{DisplayName, Test}
+import org.junit.jupiter.api.Assertions._
+
+import org.slf4j.LoggerFactory
+
+
 class StoreRocksDBTests {
+  val logger = LoggerFactory.getLogger(classOf[StoreRocksDBTests])
+
+  val storeRocksDB: StoreRocksDB = StoreRocksDB()
+
+  @Test
+  @DisplayName("test rocks db is Open")
+  def testRocksDBStatus(): Unit = {
+    assertEquals(true, storeRocksDB.isOpen)
+  }
 
 }
